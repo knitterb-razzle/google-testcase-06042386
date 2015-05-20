8f4861c9e022daf31a426f5dff5a8ed50df4b449
Brandon Knitter <knitterb@razzle.mobi>
2015-05-20 13:46:31 -0700

    Added debug to the processing

--- src/main/java/com/wolfapp/testcase/ImageProcess.java
+++ src/main/java/com/wolfapp/testcase/ImageProcess.java
@@ -2,6 +2,7 @@ package com.wolfapp.testcase;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.logging.Logger;
 
 import com.google.appengine.api.images.Image;
 import com.google.appengine.api.images.ImagesService;
@@ -16,6 +17,7 @@ import com.google.appengine.tools.cloudstorage.GcsServiceFactory;
 import com.google.appengine.tools.cloudstorage.RetryParams;
 
 public class ImageProcess {
+	private static final Logger log = Logger.getLogger(ImageProcess.class.getName());
 
 	private static final GcsService gcsService = GcsServiceFactory.createGcsService(RetryParams.getDefaultInstance());
 
@@ -24,10 +26,12 @@ public class ImageProcess {
 		Image img = ImagesServiceFactory.makeImage(data);
 		
 		if (img.getWidth()>2000 || img.getHeight()>2000) {
+			log.warning("Size "+img.getHeight()+"x"+img.getWidth()+" -> 2000x2000");
 			Transform conversion = ImagesServiceFactory.makeResize(2000, 2000, false);
 			Image newImg = imagesService.applyTransform(conversion, img, OutputEncoding.PNG);
 			img = newImg;
 		}
+		log.info("Size "+img.getHeight()+"x"+img.getWidth());
 
 		GcsFilename file = new GcsFilename("testcase_06042386", "image.png");
 		GcsFileOptions.Builder options = new GcsFileOptions.Builder().mimeType("image/png");
